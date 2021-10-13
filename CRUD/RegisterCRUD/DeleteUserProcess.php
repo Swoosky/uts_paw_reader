88a73ad270e817e89bfbb534b4d951df0aa32d9b
Swoosky <agushusky@gmail.com>
2021-10-13 14:18:20 +0800

    first commit

---
+++ CRUD/RegisterCRUD/DeleteUserProcess.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$name = "GD4_10193_E";
+$con = mysqli_connect($host, $user, $pass, $name);
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL : " . mysqli_connect_error();
+}
